42426154eab8760ffe793c9dc3b29d30997f2ab1
emmageo3 <kanfanyemma22@gmail.com>
2022-04-12 10:10:39 +0000

    programme 2

---
+++ BoucleFor.java
@@ -0,0 +1,13 @@
+public class BoucleFor {
+
+    public static void main(String[] args)
+    {
+
+        int n = 6;
+        for(int i = 0; i > n; i++)
+        {
+            System.out.println(i);
+        }
+    }
+    
+}
